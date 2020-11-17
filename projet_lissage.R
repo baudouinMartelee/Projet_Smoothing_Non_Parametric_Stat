4a169ceb46db990f7cd20be314bd544611d48a1d
baudouinMartelee <48315946+baudouinMartelee@users.noreply.github.com>
2020-11-18 00:53:59 +0100

    ccol

--- projet_lissage.R
+++ projet_lissage.R
@@ -1,42 +1,60 @@
-# PROJET LISSAGE   
+# PROJET SMOOTHING TECHNIQUE
 
 rm(list = ls()) #We use this to clear all variables in memory
 library(stats) #load packages (stats normally loaded by default)
 library(datasets) #We use some Inbuilt Data later
 
+# We create our data
 set.seed(22.1)
-#Size of our data
 n <- 100
-# Sequence of n numbers between 0 and 1 . These points are fixed
 X <- seq(from = 1/n, to = 1, by = 1/n)
-
-# this is the m(x) function 
 m <- function(x) 3 * (x)^(1/4) * cos(1.2/(x + 0.05))
-
-# theta squared function 
 theta_squared <- function(x) 2 + sin(2* pi *x)
-
-#Epsilon 
 epsilon <- rnorm(n, mean = 0, sd = 1)
-#function Y 
 set.seed(22.2)
 Y <- m(X) + 0.5 * sqrt(theta_squared(X)) * epsilon
-
+Y
 # Plot of the true function
+x <- seq(0,1, length.out = 100)
 plot(X, Y, pch = "+")
 rug(X)
-x <- seq(0,1, length.out = 300)
-lines(x, m(x), col=2)
-lines(X,m(X), col=4)
+lines(X, m(X), col=3)
 
+# Plot the true  variance theta^2
+plot(X,theta_squared(X), pch = "+" ,type = "l", ylab = "Variance")
+
+
+################################################
+#   CHOOSING A GOOD BANDWITH
+################################################
+
+#Rule of Thumb estimator
+h2 <- bw.nrd(X)
+
+#cross validation estimator
+h3 <- bw.ucv(X)
+
+# SHeater & Jones bandwith selector 
+h_dpi <- bw.SJ(X,method = "dpi")
+h_ste <- bw.SJ(X)
+
+plot(range(X), c(0,1), type="n")
+lines(density(X),col = 1)
+lines(density(X, n=100, from=min(X), to=max(X), bw="nrd", kernel = "gaussian"), col=2)
+lines(density(X, n=100, from=min(X), to=max(X), bw="ucv", kernel = "gaussian"), col=4)
+lines(density(X, n=100, from=min(X), to=max(X), bw="SJ", kernel = "gaussian"), col=5)
+legend("center", legend=c("default", "nrd", "ucv", "SJ"),col = c(1,2, 4, 5), lty=c(1,1,1), rug(X),cex = 0.5)
+rug(X)
 ################################################
 #   COMPARAISON OF DIFFERENT ESTIMATORS FOR FUNCTION M(X)
 ################################################
 
-# bandwith h for the estimation
-h = 0.02
+# A KERNEL ESTIMATOR
+############################
+
+h = h_ste
 # Nadaraya-Watson  estimator
-nWEstim <- function(x, X, Y, h, K) sum(Y * K((x - X)/h))/sum(K((x - X)/h))
+nwRegEstim <- function(x, X, Y, h, K) sum(Y * K((x - X)/h))/sum(K((x - X)/h))
 
 # Kernels CDF
 Knorm <- function(u) dnorm(u) #Gaussian kernel
@@ -45,14 +63,45 @@ Kepan <- function(u) 0.75 * (1 - u^2) * (abs(u) <= 1) #Epanechnikov kernel
 Ktria <- function(u) (1 - abs(u)) * (abs(u) <= 1) #Triangle kernel
 
 
-NWnormEst <- sapply(x, function(x) nwRegEst(x, X, Y, h, Knorm))
+NWnormEst <- sapply(x, function(x) nwRegEstim(x, X, Y, h, Knorm))
 
 plot(X, Y, pch="+", col = "black")
-lines(x, NWnormEst, type = "l", col = 4)
+lines(X, NWnormEst, type = "l", col = 4)
 lines(x, m(x), type = "l", col = 3)
 legend("topleft", legend = c("NW", "True"),
        col = c( 4, 3),
        lty = c( 1, 1), cex = 0.5)
+
+
+# A NON-KERNEL ESTIMATOR (SPLINE SMOOTHING)
+#####################################
+
+
+################################################
+#  ESTIMATORS FOR VARIANCE FUNCTION
+################################################
+
+# FAN et YAO
+epsilonEstim = Y - NWnormEst
+
+varRegEstim <- function(x, X, Y, h, K) sum(epsilonEstim^2 * K((x - X)/h))/sum(K((x - X)/h))
+
+varNormRegEstim <- sapply(x,function(x)  varRegEstim(x, X, Y, h, Knorm))
+
+VarianceX <- theta_squared(X)
+
+plot(X, VarianceX, type = "l", col = "black")
+plot(X, varNormRegEstim, type = "l", col = "black")
+lines(X, varNormRegEstim , type = "l", col = 4)
+lines(x, varNormRegEstim, type = "l", col = 3)
+legend("topleft", legend = c("TRUE VARIANCE", "VARIANCE ESTIMATOR"),
+       col = c( 4, 3),
+       lty = c( 1, 1), cex = 0.5)
+
+################################################
+# plot the estimate function
+################################################
+
 
 
 
