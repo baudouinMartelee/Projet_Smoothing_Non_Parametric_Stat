370373b9af96cb60d469e1cce0eb17bbafd1a78a
baudouinMartelee <48315946+baudouinMartelee@users.noreply.github.com>
2020-12-02 12:31:57 +0100

    final

--- projet_lissage.R
+++ projet_lissage.R
@@ -11,14 +11,11 @@ n <- 100
 
 X <- seq(from = 1/n, to = 1, by = 1/n)
 m <- function(x) 3 * (x)^(1/4) * cos(1.2/(x + 0.05))
-variance <- function(X) { 
-  var <- 2 + sin(2*pi*X) 
-  return(var)
-}
+variance <- function(x)  2 + sin(2*pi*x) 
 variance
 sigma_squared <- function(x) 2 + sin(2*pi*x)
 epsilon <- rnorm(n, mean = 0, sd = 1)
-set.seed(22.02)
+#set.seed(22.02)
 Y <- m(X) + 0.5 * sqrt(sigma_squared(X)) * epsilon
 Y
 # Plot of observations and m(x) function.
@@ -135,7 +132,7 @@ legend("bottomright", legend=c("True m(x)","spline default (Cross-Validation)","
 
 estimateVariance <- function(x, X, Y, h1, h2, RegEstim, K){
   epsilonEstim = 2*(Y - sapply(x, function(x) RegEstim(x, X, Y, h1, K)))
-  fanAndYaoEstim <- sum(epsilonEstim^2 * K((x - X)/h2))/sum(K((x - X)/h2))
+  fanAndYaoEstim <- sum((epsilonEstim^2) * K((x - X)/h2))/sum(K((x - X)/h2))
   return(fanAndYaoEstim)
 }
 
@@ -163,7 +160,10 @@ plot(X, VarianceX, type = "l",ylim = c(0,15), col = "black", main= paste("Compar
 lines(x, varianceNW1, type = "l", col = 2)
 lines(x, varianceNW2, type = "l", col = 3)
 lines(x, varianceNW3, type = "l", col = 4)
-lines(x, varianceNW32, type = "l", col = 5)
+lines(x, varianceNW4, type = "l", col = 5)
+lines(x, varianceNW5, type = "l", col = 2)
+lines(x, varianceNW6, type = "l", col = 3)
+lines(x, varianceNW7, type = "l", col = 4)
 
 
 legend("topright", legend = c("True Variance", "variance NW, h1 = gl,h2 = lo","variance NW, h1 = gl,h2 = RT","variance NW, h1 = lo,h2 = gl","variance NW, h1 = lo,h2 = RT","variance NW, h1 = RT,h2 = gl","variance NW, h1 = RT,h2 = lo"),
@@ -175,6 +175,9 @@ lines(x, varianceGM1, type = "l", col = 2)
 lines(x, varianceGM2, type = "l", col = 3)
 lines(x, varianceGM3, type = "l", col = 4)
 lines(x, varianceGM32, type = "l", col = 5)
+lines(x, varianceGM4, type = "l", col = 5)
+lines(x, varianceGM5, type = "l", col = 5)
+lines(x, varianceGM6, type = "l", col = 5)
 
 legend("topright", legend = c("True Variance", "variance NW, h1 = gl,h2 = lo","variance NW, h1 = gl,h2 = RT","variance NW, h1 = lo,h2 = gl","variance NW, h1 = lo,h2 = RT","variance NW, h1 = RT,h2 = gl","variance NW, h1 = RT,h2 = lo"),
        col = c( 1,2,3,4,5),
@@ -182,7 +185,7 @@ legend("topright", legend = c("True Variance", "variance NW, h1 = gl,h2 = lo","v
 
 # variance for spline smoothing
 
-estimateVarianceSMCV <- function(x, X, Y,h2, K){
+estimateVarianceSMCV <- function(x, X, Y, h2, K){
   epsilonEstim = 2*(Y - smooth.spline(X, Y)$y)
   fanAndYaoEstim <- sum(epsilonEstim^2 * K((x - X)/h2))/sum(K((x - X)/h2))
   return(fanAndYaoEstim)
@@ -218,8 +221,8 @@ lines(x, varianceSM_hRT_hRT, col=8)
 lines(x, varianceSM_hRT_hgl, col=9)
 lines(x, varianceSM_hRT_hlo, col=10)
 
-legend("topright", legend=c("spline(Cross Validation, h_gl)","spline(Cross Validation, h_lo)","spline(Cross Validation, h_RT)"),
-       col = c(3,5),lty=c(1,1), cex=0.8)
+legend("topright", legend=c("true Variance","h1 = CV, h2 = h_gl","h1 = CV, h2 = h_lo","h1 = CV, h2 = h_RT","h1 = h_gl, h2 = h_gl","h1 = h_gl, h2 = h_lo","h1 = h_gl, h2 = h_RT","h1 = h_RT, h2 = h_RT","h1 = h_RT, h2 = h_gl","h1 = h_RT, h2 = h_lo"),
+       col = c(1,2,3,4,5,6,7,8,9,10),lty=c(1,1,1,1,1,1,1,1,1,1), cex=0.8)
 
 ################################################
 #   MONTE CARLO SIMULATION 
@@ -237,6 +240,7 @@ GM_simulation <- function(x, sigma_squared, m, K, iter, h1, h2, regEstim){
   
   GMvarianceEstim <- matrix(NA, nrow = iter, ncol = length(x))
   GMvariance <- matrix(NA, nrow = iter, ncol = length(x))
+  
   for (k in 1:iter) {
     set.seed(k)
     epsilon <- rnorm(n, mean = 0, sd = 1)
@@ -246,47 +250,128 @@ GM_simulation <- function(x, sigma_squared, m, K, iter, h1, h2, regEstim){
       GMvarianceEstim[k,i] <- estimateVariance (x[i], X, Y, h1, h2, regEstim, K)
       GMvariance[k,i] <- sigma_squared(x[i])
     }
-    
-    SE = matrix(NA, nrow = iter, ncol = length(x))
-    for (i in 1:length(x)) {
-      SE[k,i] = (GMvarianceEstim[k,i]-GMvariance[k,i])^2
-    }  
   }
-  
- 
-  
+  SE = matrix(NA, nrow = iter, ncol = length(x))
+  for (i in 1:length(x)) {
+    SE[,i] = (GMvarianceEstim[,i]-GMvariance[,i])^2
+  }  
+
   meanGMvarianceEstim <- colMeans(GMvarianceEstim)
   meanGMvariance <- colMeans(GMvariance)
   #cat(class(meanGMvarianceEstim))
-  print.table(meanGMvarianceEstim)
-  print.table(meanGMvariance)
+  #print.table(meanGMvarianceEstim)
+  #print.table(meanGMvariance)
   biais = meanGMvarianceEstim - meanGMvariance
+  variance<- colMeans(GMvarianceEstim^2)-meanGMvarianceEstim^2
+  
+  MSE = colMeans(SE)
+  MSSE = mean(MSE) # MSSE as an estimation of MISE in simulation
+  #plot(x, biais, type="l", col = 2)
+  #abline(h=0)
+  my_list <- list("biais" = biais,"IMSE" =  MSSE,"variance" = variance)
+  return(my_list)
+}
+
+
+#GM biais 
+GMresult_25 <- GM_simulation(x, sigma_squared, m, cdfKnorm, 25, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)
+GMresult_50 <- GM_simulation(x, sigma_squared, m, cdfKnorm, 50, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)
+GMresult_100 <- GM_simulation(x, sigma_squared, m, cdfKnorm, 100, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)
+GMresult_500 <- GM_simulation(x, sigma_squared, m, cdfKnorm, 500, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)
+GMresult_1000 <- GM_simulation(x, sigma_squared, m, cdfKnorm, 1000, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)
+
+
+GMresult_25$biais
+GMresult_25$variance
+cat("IMSE with Gasser-Müller : ",GMresult_1000$IMSE)
+
+GMresult_1000$biais
+
+#MSE <- GM_simulation(x, sigma_squared, m, cdfKnorm, 25, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)[[2]]
+
+#variance <- GM_simulation(x, sigma_squared, m, cdfKnorm, 25, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)[[3]]
+
+
+plot_stat <- function(stat, method,stat25, stat50, stat100, stat500, stat1000){
+  plot(X, stat25, type = "l", col = "black", main=paste("", stat," de l'estimateur ", method," de la variance ", sep=" "))
+  lines(x, stat50, col=2)
+  lines(x, stat100, col=3)
+  lines(x, stat500, col=4)
+  lines(x, stat1000, col=5)
+  legend("topright", legend=c("25 iterations","50 iterations","100 iterations","500 iterations","1000 iterations"),
+         col = c(1,2,3,4,5),lty=c(1,1,1,1,1), cex=0.8)
+}
+
+plot_stat("Biais","GM",GMresult_25$biais, GMresult_50$biais, GMresult_100$biais, GMresult_500$biais, GMresult_1000$biais)
+plot_stat("Variance","GM",GMresult_25$variance, GMresult_50$variance, GMresult_100$variance, GMresult_500$variance, GMresult_1000$variance)
+
+#NW biais 
+NWresult_25 <- GM_simulation(x, sigma_squared, m, Knorm, 25, h_gl$bandwidth, h_gl$bandwidth, nwRegEstim)
+NWresult_50 <- GM_simulation(x, sigma_squared, m, Knorm, 50, h_gl$bandwidth, h_gl$bandwidth, nwRegEstim)
+NWresult_100 <- GM_simulation(x, sigma_squared, m, Knorm, 100, h_gl$bandwidth, h_gl$bandwidth, nwRegEstim)
+NWresult_500 <- GM_simulation(x, sigma_squared, m, Knorm, 500, h_gl$bandwidth, h_gl$bandwidth, nwRegEstim)
+NWresult_1000 <- GM_simulation(x, sigma_squared, m, Knorm, 1000, h_gl$bandwidth, h_gl$bandwidth, nwRegEstim)
+
+
+plot_stat("Biais","NW",NWresult_25$biais, NWresult_50$biais, NWresult_100$biais, NWresult_500$biais, NWresult_1000$biais)
+plot_stat("Variance","NW",NWresult_25$variance, NWresult_50$variance, NWresult_100$variance, NWresult_500$variance, NWresult_1000$variance)
+
+cat("IMSE with GNadarya-Watson : ",NWresult_1000$IMSE)
+
+
+# Spline smoothing simulation avec CV
+n <- 100 # number of observations
+x <- seq(from = 0, to = 1, length = 100) #a sequence of points
+m <- function(x) 3 * (x)^(1/4) * cos(1.2/(x + 0.05)) #true regression model
+epsilon <- rnorm(n, mean = 0, sd = 1)
+SM_simulation <- function(x, sigma_squared, m, K, iter, h2){
+  
+  SMvarianceEstim <- matrix(NA, nrow = iter, ncol = length(x))
+  SMvariance <- matrix(NA, nrow = iter, ncol = length(x))
+  for (k in 1:iter) {
+    set.seed(k)
+    epsilon <- rnorm(n, mean = 0, sd = 1)
+    set.seed(k+1)
+    Y <- m(X) + 0.5 * sqrt(sigma_squared(X)) * epsilon
+    for (i in 1:length(x)) {
+      SMvarianceEstim[k,i] <- estimateVarianceSMCV (x[i], X, Y, h2, K)
+      SMvariance[k,i] <- sigma_squared(x[i])
+    }
+  }
+  
+  SE = matrix(NA, nrow = iter, ncol = length(x))
+  for (i in 1:length(x)) {
+    SE[,i] = (SMvarianceEstim[,i]-SMvariance[,i])^2
+  }  
+  
+  meanSMvarianceEstim <- colMeans(SMvarianceEstim)
+  meanSMvariance <- colMeans(SMvariance)
+  #cat(class(meanGMvarianceEstim))
+  print.table(meanSMvarianceEstim)
+  print.table(meanSMvariance)
+  biais = meanSMvarianceEstim - meanSMvariance
   cat(biais)
   #variance1<-colMeans((NWnormEst-NW)^2)
   #plot(x,variance1 ,ylab="Variance",type="l",col="#C72C48")
+  variance<- colMeans(SMvarianceEstim^2)-meanSMvarianceEstim^2
   MSE = colMeans(SE)
   MSSE = mean(MSE) # MSSE as an estimation of MISE in simulation
   
   
   #plot(x, biais, type="l", col = 2)
   #abline(h=0)
-  my_list <- list("biais" = biais,"MSE" =  MSE,"variance" = variance)
+  my_list <- list("biais" = biais,"IMSE" =  MSSE,"variance" = variance)
   return(my_list)
 }
 
-library(gsubfn)
-GMresult <- GM_simulation(x, sigma_squared, m, cdfKnorm, 25, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)
-GMresult$biais
+SMresult_25 <- SM_simulation(x, sigma_squared, m, Knorm, 25, h_gl$bandwidth)
+SMresult_50 <- SM_simulation(x, sigma_squared, m, Knorm, 50, h_gl$bandwidth)
+SMresult_100 <- SM_simulation(x, sigma_squared, m, Knorm, 100, h_gl$bandwidth)
+SMresult_500 <- SM_simulation(x, sigma_squared, m, Knorm, 500, h_gl$bandwidth)
+SMresult_1000 <- SM_simulation(x, sigma_squared, m, Knorm, 1000, h_gl$bandwidth)
 
-MSE <- GM_simulation(x, sigma_squared, m, cdfKnorm, 25, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)[[2]]
-
-variance <- GM_simulation(x, sigma_squared, m, cdfKnorm, 25, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)[[3]]
-
-
-
+plot_stat("Biais","SM",SMresult_25$biais, SMresult_50$biais, SMresult_100$biais, SMresult_500$biais, SMresult_1000$biais)
+plot_stat("Variance","SM",SMresult_25$variance, SMresult_50$variance, SMresult_100$variance, SMresult_500$variance, SMresult_1000$variance)
 
 
-
-
-
-
+cat("IMSE with Spline smoothing : ",SMresult_1000$IMSE)
