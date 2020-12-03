233c733ce431402bf9e5f75d81c9e81da6ae5433
baudouinMartelee <48315946+baudouinMartelee@users.noreply.github.com>
2020-12-03 12:55:05 +0100

    kjfeo

--- projet_lissage.R
+++ projet_lissage.R
@@ -20,10 +20,11 @@ Y <- m(X) + 0.5 * sqrt(sigma_squared(X)) * epsilon
 Y
 # Plot of observations and m(x) function.
 x <- seq(0,1, length.out = 100)
+
 plot(X, Y, pch =20, lwd=0.2)
 rug(X)
-lines(X, m(X), col=3)
-legend("top", legend = "m(x)", col = "green",lty=c(1,1),cex=1)
+lines(X, m(X), col=1)
+legend("bottomright", legend = "m(x)", col = "black",lty=c(1,1),cex=1)
 
 # Plot the true  variance theta^2
 plot(X, sigma_squared(X), type = "l", ylab = "Variance sigma2")
@@ -43,7 +44,7 @@ h_lo <- lokerns(X, Y, hetero = TRUE, sig = sigma_squared(X), n.out = 100)
 h_lo$bandwidth
 #Rule of Thumb estimator
 h_RT <- bw.nrd(X)
-
+h_RT
 #Pour  prendre la bandwidth
 
 ################################################
@@ -64,14 +65,14 @@ NWnormEst_h_gl <- sapply(x, function(x) nwRegEstim(x, X, Y, h_gl$bandwidth, Knor
 NWnormEst_h_lo <- sapply(x, function(x) nwRegEstim(x, X, Y, h_lo$bandwidth, Knorm))
 NWnormEst_h_RT <- sapply(x, function(x) nwRegEstim(x, X, Y, h_RT, Knorm))
 
-plot(X, Y, pch=20, col = "black", main = "Comparaison m(x) avec différents bandwidths pour Nadarya-Watson")
-lines(x, m(x), type = "l", col = 3)
-lines(X, NWnormEst_h_gl, type = "l", col = 4)
-lines(X, NWnormEst_h_lo, type = "l", col = 2)
-lines(X, NWnormEst_h_RT, type = "l", col = 5)
+plot(X, Y, pch=20, col = "black", main = "Estimateurs de m(x) avec Nadarya-Watson")
+lines(x, m(x), type = "l", col = 1)
+lines(x, NWnormEst_h_gl, type = "l", col = 2)
+lines(x, NWnormEst_h_lo, type = "l", col = 3)
+lines(x, NWnormEst_h_RT, type = "l", col = 4)
 legend("bottomright", legend = c("True m(x)", "NW, h global = 0.05","NW, h local","NW, h Thumb of rule = 0.12"),
-       col = c( 3, 4, 2, 5),
-       lty = c( 1, 1))
+       col = c( 1, 2, 3, 4),
+       lty = c( 1, 1), cex = 0.8)
 
 #Gass and Muller estimator
 cdfKnorm <- function(u) pnorm(u) #Gaussian kernel cdf
@@ -97,13 +98,13 @@ GMnormEst_h_lo <- sapply(x, function(x) gmRegEstim(x, X, Y, h_lo$bandwidth, cdfK
 GMnormEst_h_RT <- sapply(x, function(x) gmRegEstim(x, X, Y, h_RT, cdfKnorm ))
 
 
-plot(X, Y, pch=20, col = "black", main = "Comparaison m(x) avec différents bandwidths pour Gasser & Muller")
-lines(x, m(x), type = "l", col = 3)
-lines(X, GMnormEst_h_gl, type = "l", col = 4)
-lines(X, GMnormEst_h_lo, type = "l", col = 2)
-lines(X, GMnormEst_h_RT, type = "l", col = 5)
+plot(X, Y, pch=20, col = "black", main = "Estimateurs de m(x) avec Gasser & Muller")
+lines(x, m(x), type = "l", col = 1)
+lines(x, GMnormEst_h_gl, type = "l", col = 2)
+lines(x, GMnormEst_h_lo, type = "l", col = 3)
+lines(x, GMnormEst_h_RT, type = "l", col = 4)
 legend("bottomright", legend = c("True m(x)", "GM, h global = 0.05","GM, h local","GM, h Thumb of rule = 0.12"),
-       col = c( 3, 4, 2),
+       col = c(1, 2, 3, 4),
        lty = c( 1, 1), cex = 0.8)
 
 # A NON-KERNEL ESTIMATOR (SPLINE SMOOTHING)
@@ -115,13 +116,13 @@ spl2 <- smooth.spline(X, Y, spar=h_gl$bandwidth)
 spl3 <- smooth.spline(X, Y, spar=h_RT)# smoothing parameter = h_RT
 
 ## Visualization
-plot(X, Y, pch=20, col = "black", main="Spline Smoothing")
-lines(x, m(x), col=1, lty=2)
+plot(X, Y, pch=20, col = "black", main="Estimateurs de m(x) avec Spline Smoothing")
+lines(x, m(x), col=1)
 lines(predict(spl0,x), col=2)
 lines(predict(spl2,x), col=3)
 lines(predict(spl3,x), col=4)
 legend("bottomright", legend=c("True m(x)","spline default (Cross-Validation)","spline(spar=h_gl)","spline(spar=h_RT)"),
-       col = c(1,2,3,4),lty=c(2,1,1,1,1), cex=1)
+       col = c(1,2,3,4),lty=c(1,1,1,1,1), cex=0.8)
 
 ################################################
 #  ESTIMATORS FOR VARIANCE FUNCTION
@@ -131,69 +132,76 @@ legend("bottomright", legend=c("True m(x)","spline default (Cross-Validation)","
 # 1for estimate m(x) and 1 for estimate sigma2.
 
 estimateVariance <- function(x, X, Y, h1, h2, RegEstim, K){
-  epsilonEstim = 2*(Y - sapply(x, function(x) RegEstim(x, X, Y, h1, K)))
+  epsilonEstim = Y - sapply(x, function(x) RegEstim(x, X, Y, h1, K))
   fanAndYaoEstim <- sum((epsilonEstim^2) * K((x - X)/h2))/sum(K((x - X)/h2))
   return(fanAndYaoEstim)
 }
 
-varianceNW1 <-sapply(x, function(x) estimateVariance(x, X, Y, h_gl$bandwidth, h_gl$bandwidth, nwRegEstim, Knorm))
-varianceNW2 <-sapply(x, function(x) estimateVariance(x, X, Y, h_gl$bandwidth, h_lo$bandwidth, nwRegEstim, Knorm))
-varianceNW2 <-sapply(x, function(x) estimateVariance(x, X, Y, h_gl$bandwidth, h_lo$bandwidth, nwRegEstim, Knorm))
-varianceNW3 <-sapply(x, function(x) estimateVariance(x, X, Y, h_lo$bandwidth, h_gl$bandwidth, nwRegEstim, Knorm))
-varianceNW4 <-sapply(x, function(x) estimateVariance(x, X, Y, h_lo$bandwidth, h_lo$bandwidth, nwRegEstim, Knorm))
-varianceNW5 <-sapply(x, function(x) estimateVariance(x, X, Y, h_lo$bandwidth, h_RT, nwRegEstim, Knorm))
-varianceNW6 <-sapply(x, function(x) estimateVariance(x, X, Y, h_RT, h_gl$bandwidth, nwRegEstim, Knorm))
-varianceNW7 <-sapply(x, function(x) estimateVariance(x, X, Y, h_RT, h_lo$bandwidth, nwRegEstim, Knorm))
+varianceNW11 <-sapply(x, function(x) estimateVariance(x, X, Y, h_gl$bandwidth, h_gl$bandwidth, nwRegEstim, Knorm))
+varianceNW12 <-sapply(x, function(x) estimateVariance(x, X, Y, h_gl$bandwidth, h_lo$bandwidth, nwRegEstim, Knorm))
+varianceNW12 <-sapply(x, function(x) estimateVariance(x, X, Y, h_gl$bandwidth, h_RT, nwRegEstim, Knorm))
+varianceNW21 <-sapply(x, function(x) estimateVariance(x, X, Y, h_lo$bandwidth, h_gl$bandwidth, nwRegEstim, Knorm))
+varianceNW22 <-sapply(x, function(x) estimateVariance(x, X, Y, h_lo$bandwidth, h_lo$bandwidth, nwRegEstim, Knorm))
+varianceNW23 <-sapply(x, function(x) estimateVariance(x, X, Y, h_lo$bandwidth, h_RT, nwRegEstim, Knorm))
+varianceNW31 <-sapply(x, function(x) estimateVariance(x, X, Y, h_RT, h_gl$bandwidth, nwRegEstim, Knorm))
+varianceNW32 <-sapply(x, function(x) estimateVariance(x, X, Y, h_RT, h_lo$bandwidth, nwRegEstim, Knorm))
+varianceNW33 <-sapply(x, function(x) estimateVariance(x, X, Y, h_RT, h_RT, nwRegEstim, Knorm))
 
-varianceGM1 <-sapply(x, function(x) estimateVariance(x, X, Y, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim, cdfKnorm))
-varianceGM2 <-sapply(x, function(x) estimateVariance(x, X, Y, h_gl$bandwidth, h_lo$bandwidth, gmRegEstim, cdfKnorm))
-varianceGM3 <-sapply(x, function(x) estimateVariance(x, X, Y, h_lo$bandwidth, h_gl$bandwidth, gmRegEstim, cdfKnorm))
-varianceGM32 <-sapply(x, function(x) estimateVariance(x, X, Y, h_lo$bandwidth, h_lo$bandwidth, gmRegEstim, cdfKnorm))
-varianceGM4 <-sapply(x, function(x) estimateVariance(x, X, Y, h_lo$bandwidth, h_RT, gmRegEstim, cdfKnorm))
-varianceGM5 <-sapply(x, function(x) estimateVariance(x, X, Y, h_RT, h_gl$bandwidth, gmRegEstim, cdfKnorm))
-varianceGM6 <-sapply(x, function(x) estimateVariance(x, X, Y, h_RT, h_lo$bandwidth, gmRegEstim, cdfKnorm))
+
+varianceGM11 <-sapply(x, function(x) estimateVariance(x, X, Y, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim, cdfKnorm))
+varianceGM12 <-sapply(x, function(x) estimateVariance(x, X, Y, h_gl$bandwidth, h_lo$bandwidth, gmRegEstim, cdfKnorm))
+varianceGM12 <-sapply(x, function(x) estimateVariance(x, X, Y, h_gl$bandwidth, h_RT, gmRegEstim, cdfKnorm))
+varianceGM21 <-sapply(x, function(x) estimateVariance(x, X, Y, h_lo$bandwidth, h_gl$bandwidth, gmRegEstim, cdfKnorm))
+varianceGM22 <-sapply(x, function(x) estimateVariance(x, X, Y, h_lo$bandwidth, h_lo$bandwidth, gmRegEstim, cdfKnorm))
+varianceGM23 <-sapply(x, function(x) estimateVariance(x, X, Y, h_lo$bandwidth, h_RT, gmRegEstim, cdfKnorm))
+varianceGM31 <-sapply(x, function(x) estimateVariance(x, X, Y, h_RT, h_gl$bandwidth, gmRegEstim, cdfKnorm))
+varianceGM32 <-sapply(x, function(x) estimateVariance(x, X, Y, h_RT, h_lo$bandwidth, gmRegEstim, cdfKnorm))
+varianceGM33 <-sapply(x, function(x) estimateVariance(x, X, Y, h_RT, h_RT, gmRegEstim, cdfKnorm))
+
 
 VarianceX <- sigma_squared(X)
 
 # PLOT VARIANCE
-plot(X, VarianceX, type = "l",ylim = c(0,15), col = "black", main= paste("Comparaison de Variance en utilisant la méthode Nadarya-Watson"))
-lines(x, varianceNW1, type = "l", col = 2)
-lines(x, varianceNW2, type = "l", col = 3)
-lines(x, varianceNW3, type = "l", col = 4)
-lines(x, varianceNW4, type = "l", col = 5)
-lines(x, varianceNW5, type = "l", col = 2)
-lines(x, varianceNW6, type = "l", col = 3)
-lines(x, varianceNW7, type = "l", col = 4)
+plot(X, VarianceX, type = "l",ylim = c(0,5),ylab = "Variance de X", col = "black", main= paste("Comparaison de variances en utilisant la méthode Nadarya-Watson"))
+lines(x, varianceNW11, type = "l", col = 2)
+lines(x, varianceNW12, type = "l", col = 3)
+lines(x, varianceNW13, type = "l", col = 4)
+lines(x, varianceNW21, type = "l", col = 5)
+lines(x, varianceNW22, type = "l", col = 6)
+lines(x, varianceNW23, type = "l", col = 7)
+lines(x, varianceNW31, type = "l", col = 8)
+lines(x, varianceNW32, type = "l", col = 9)
+lines(x, varianceNW33, type = "l", col = 10)
+legend("topright", legend = c("True Variance","h1 =h_gl, h2 =h_gl","h1 =h_gl, h2 =h_lo","h1 =h_gl, h2 =h_RT","h1 =h_lo, h2 =h_gl","h1 =h_lo, h2 =h_lo","h1 =h_lo, h2 =h_RT","h1 =h_RT, h2 =h_gl","h1 =h_RT, h2 =h_lo","h1 =h_RT, h2 =h_RT"),
+       col = c(1,2,3,4,5,6,7,8,9,10),
+       lty = c(1, 1), cex = 0.8)
 
 
-legend("topright", legend = c("True Variance", "variance NW, h1 = gl,h2 = lo","variance NW, h1 = gl,h2 = RT","variance NW, h1 = lo,h2 = gl","variance NW, h1 = lo,h2 = RT","variance NW, h1 = RT,h2 = gl","variance NW, h1 = RT,h2 = lo"),
-       col = c( 1,2,3,4,5),
-       lty = c( 1, 1), cex = 0.5)
-
-plot(X, VarianceX, type = "l",ylim = c(0,30), col = "black", main= paste("Comparaison de Variance en utilisant la méthode Gasser and Muller"))
-lines(x, varianceGM1, type = "l", col = 2)
-lines(x, varianceGM2, type = "l", col = 3)
-lines(x, varianceGM3, type = "l", col = 4)
-lines(x, varianceGM32, type = "l", col = 5)
-lines(x, varianceGM4, type = "l", col = 5)
-lines(x, varianceGM5, type = "l", col = 5)
-lines(x, varianceGM6, type = "l", col = 5)
-
-legend("topright", legend = c("True Variance", "variance NW, h1 = gl,h2 = lo","variance NW, h1 = gl,h2 = RT","variance NW, h1 = lo,h2 = gl","variance NW, h1 = lo,h2 = RT","variance NW, h1 = RT,h2 = gl","variance NW, h1 = RT,h2 = lo"),
-       col = c( 1,2,3,4,5),
-       lty = c( 1, 1), cex = 0.5)
+plot(X, VarianceX, type = "l", ylim=c(0,7), ylab = "Variance de X", col = "black", main= paste("Comparaison de variances en utilisant la méthode Gasser and Muller"))
+lines(x, varianceGM11, type = "l", col = 2)
+lines(x, varianceGM12, type = "l", col = 3)
+lines(x, varianceGM13, type = "l", col = 4)
+lines(x, varianceGM21, type = "l", col = 5)
+lines(x, varianceGM22, type = "l", col = 6)
+lines(x, varianceGM23, type = "l", col = 7)
+lines(x, varianceGM31, type = "l", col = 8)
+lines(x, varianceGM32, type = "l", col = 9)
+lines(x, varianceGM33, type = "l", col = 10)
+legend("topright", legend = c("True Variance","h1 =h_gl, h2 =h_gl","h1 =h_gl, h2 =h_lo","h1 =h_gl, h2 =h_RT","h1 =h_lo, h2 =h_gl","h1 =h_lo, h2 =h_lo","h1 =h_lo, h2 =h_RT","h1 =h_RT, h2 =h_gl","h1 =h_RT, h2 =h_lo","h1 =h_RT, h2 =h_RT"),
+       col = c( 1,2,3,4,5,6,7,8,9,10),
+       lty = c( 1, 1), cex = 0.8)
 
 # variance for spline smoothing
 
 estimateVarianceSMCV <- function(x, X, Y, h2, K){
-  epsilonEstim = 2*(Y - smooth.spline(X, Y)$y)
+  epsilonEstim = Y - smooth.spline(X, Y)$y
   fanAndYaoEstim <- sum(epsilonEstim^2 * K((x - X)/h2))/sum(K((x - X)/h2))
   return(fanAndYaoEstim)
 }
 
 estimateVarianceSM <- function(x, X, Y, h1, h2, K){
   
-  epsilonEstim = 2*(Y - smooth.spline(X, Y, spar = h1)$y)
+  epsilonEstim = Y - smooth.spline(X, Y, spar = h1)$y
   fanAndYaoEstim <- sum(epsilonEstim^2 * K((x - X)/h2))/sum(K((x - X)/h2))
   return(fanAndYaoEstim)
 }
@@ -210,7 +218,7 @@ varianceSM_hRT_hlo <-sapply(x, function(x) estimateVarianceSM(x, X, Y, h_RT, h_l
 
 VarianceX <- sigma_squared(X)
 
-plot(X, VarianceX, type = "l",ylim = c(0,5), col = "black", main="Variance using Spline Smoothing")
+plot(X, VarianceX, type = "l",ylim = c(0,3.5) ,ylab = "Variance de X" , col = "black", main="Comparaison de variance en utilisant la méthode Spline Smoothing")
 lines(x, varianceSM_CV_h_gl, col=2)
 lines(x, varianceSM_CV_h_lo, col=3)
 lines(x, varianceSM_CV_h_RT, col=4)
@@ -220,7 +228,6 @@ lines(x, varianceSM_hgl_hrt, col=7)
 lines(x, varianceSM_hRT_hRT, col=8)
 lines(x, varianceSM_hRT_hgl, col=9)
 lines(x, varianceSM_hRT_hlo, col=10)
-
 legend("topright", legend=c("true Variance","h1 = CV, h2 = h_gl","h1 = CV, h2 = h_lo","h1 = CV, h2 = h_RT","h1 = h_gl, h2 = h_gl","h1 = h_gl, h2 = h_lo","h1 = h_gl, h2 = h_RT","h1 = h_RT, h2 = h_RT","h1 = h_RT, h2 = h_gl","h1 = h_RT, h2 = h_lo"),
        col = c(1,2,3,4,5,6,7,8,9,10),lty=c(1,1,1,1,1,1,1,1,1,1), cex=0.8)
 
@@ -258,38 +265,24 @@ GM_simulation <- function(x, sigma_squared, m, K, iter, h1, h2, regEstim){
 
   meanGMvarianceEstim <- colMeans(GMvarianceEstim)
   meanGMvariance <- colMeans(GMvariance)
-  #cat(class(meanGMvarianceEstim))
-  #print.table(meanGMvarianceEstim)
-  #print.table(meanGMvariance)
   biais = meanGMvarianceEstim - meanGMvariance
   variance<- colMeans(GMvarianceEstim^2)-meanGMvarianceEstim^2
   
   MSE = colMeans(SE)
   MSSE = mean(MSE) # MSSE as an estimation of MISE in simulation
-  #plot(x, biais, type="l", col = 2)
-  #abline(h=0)
   my_list <- list("biais" = biais,"IMSE" =  MSSE,"variance" = variance)
   return(my_list)
 }
 
 
 #GM biais 
-GMresult_25 <- GM_simulation(x, sigma_squared, m, cdfKnorm, 25, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)
-GMresult_50 <- GM_simulation(x, sigma_squared, m, cdfKnorm, 50, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)
-GMresult_100 <- GM_simulation(x, sigma_squared, m, cdfKnorm, 100, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)
-GMresult_500 <- GM_simulation(x, sigma_squared, m, cdfKnorm, 500, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)
-GMresult_1000 <- GM_simulation(x, sigma_squared, m, cdfKnorm, 1000, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)
+GMresult_25 <- GM_simulation(x, sigma_squared, m, cdfKnorm, 25, h_RT, h_RT, gmRegEstim)
+GMresult_50 <- GM_simulation(x, sigma_squared, m, cdfKnorm, 50, h_RT, h_RT, gmRegEstim)
+GMresult_100 <- GM_simulation(x, sigma_squared, m, cdfKnorm, 100, h_RT, h_RT, gmRegEstim)
+GMresult_500 <- GM_simulation(x, sigma_squared, m, cdfKnorm, 500, h_RT, h_RT, gmRegEstim)
+GMresult_1000 <- GM_simulation(x, sigma_squared, m, cdfKnorm, 1000, h_RT, h_RT, gmRegEstim)
 
-
-GMresult_25$biais
-GMresult_25$variance
 cat("IMSE with Gasser-Müller : ",GMresult_1000$IMSE)
-
-GMresult_1000$biais
-
-#MSE <- GM_simulation(x, sigma_squared, m, cdfKnorm, 25, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)[[2]]
-
-#variance <- GM_simulation(x, sigma_squared, m, cdfKnorm, 25, h_gl$bandwidth, h_gl$bandwidth, gmRegEstim)[[3]]
 
 
 plot_stat <- function(stat, method,stat25, stat50, stat100, stat500, stat1000){
@@ -306,11 +299,17 @@ plot_stat("Biais","GM",GMresult_25$biais, GMresult_50$biais, GMresult_100$biais,
 plot_stat("Variance","GM",GMresult_25$variance, GMresult_50$variance, GMresult_100$variance, GMresult_500$variance, GMresult_1000$variance)
 
 #NW biais 
-NWresult_25 <- GM_simulation(x, sigma_squared, m, Knorm, 25, h_gl$bandwidth, h_gl$bandwidth, nwRegEstim)
-NWresult_50 <- GM_simulation(x, sigma_squared, m, Knorm, 50, h_gl$bandwidth, h_gl$bandwidth, nwRegEstim)
-NWresult_100 <- GM_simulation(x, sigma_squared, m, Knorm, 100, h_gl$bandwidth, h_gl$bandwidth, nwRegEstim)
-NWresult_500 <- GM_simulation(x, sigma_squared, m, Knorm, 500, h_gl$bandwidth, h_gl$bandwidth, nwRegEstim)
-NWresult_1000 <- GM_simulation(x, sigma_squared, m, Knorm, 1000, h_gl$bandwidth, h_gl$bandwidth, nwRegEstim)
+NWresult_25 <- GM_simulation(x, sigma_squared, m, Knorm, 25, h_gl$bandwidth, h_lo$bandwidth, nwRegEstim)
+NWresult_50 <- GM_simulation(x, sigma_squared, m, Knorm, 50, h_gl$bandwidth, h_lo$bandwidth, nwRegEstim)
+NWresult_100 <- GM_simulation(x, sigma_squared, m, Knorm, 100, h_gl$bandwidth, h_lo$bandwidth, nwRegEstim)
+NWresult_500 <- GM_simulation(x, sigma_squared, m, Knorm, 500, h_gl$bandwidth, h_lo$bandwidth, nwRegEstim)
+NWresult_1000 <- GM_simulation(x, sigma_squared, m, Knorm, 1000, h_gl$bandwidth, h_lo$bandwidth, nwRegEstim)
+
+NWresult_25 <- GM_simulation(x, sigma_squared, m, Knorm, 25, h_RT, h_RT, nwRegEstim)
+NWresult_50 <- GM_simulation(x, sigma_squared, m, Knorm, 50, h_RT, h_RT, nwRegEstim)
+NWresult_100 <- GM_simulation(x, sigma_squared, m, Knorm, 100, h_RT, h_RT, nwRegEstim)
+NWresult_500 <- GM_simulation(x, sigma_squared, m, Knorm, 500, h_RT, h_RT, nwRegEstim)
+NWresult_1000 <- GM_simulation(x, sigma_squared, m, Knorm, 1000, h_RT, h_RT, nwRegEstim)
 
 
 plot_stat("Biais","NW",NWresult_25$biais, NWresult_50$biais, NWresult_100$biais, NWresult_500$biais, NWresult_1000$biais)
@@ -346,20 +345,10 @@ SM_simulation <- function(x, sigma_squared, m, K, iter, h2){
   
   meanSMvarianceEstim <- colMeans(SMvarianceEstim)
   meanSMvariance <- colMeans(SMvariance)
-  #cat(class(meanGMvarianceEstim))
-  print.table(meanSMvarianceEstim)
-  print.table(meanSMvariance)
   biais = meanSMvarianceEstim - meanSMvariance
-  cat(biais)
-  #variance1<-colMeans((NWnormEst-NW)^2)
-  #plot(x,variance1 ,ylab="Variance",type="l",col="#C72C48")
   variance<- colMeans(SMvarianceEstim^2)-meanSMvarianceEstim^2
   MSE = colMeans(SE)
   MSSE = mean(MSE) # MSSE as an estimation of MISE in simulation
-  
-  
-  #plot(x, biais, type="l", col = 2)
-  #abline(h=0)
   my_list <- list("biais" = biais,"IMSE" =  MSSE,"variance" = variance)
   return(my_list)
 }
